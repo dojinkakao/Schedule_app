f635ded4b29ab6017e097f42c1213a4e1949c638
dojinkakao <megumelody.xoxo@icloud.con>
2022-06-22 08:17:00 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'post#index'
+  get 'post/new'
+  get 'post/show/:id/' => 'post#show'
+
+  resources :posts, only: [:new, :create, :index]
 end
